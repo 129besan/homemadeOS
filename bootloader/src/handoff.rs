f05cefe8b12bc8b842d3a17c9b0d4a8f22c04501
yusa129be <yusawatanabe8@gmail.com>
2026-01-09 09:00:00 +0900

    boot: define stable BootInfo ABI

---
+++ bootloader/src/handoff.rs
@@ -0,0 +1,35 @@
+#[repr(C)]
+pub struct BootInfo {
+    pub memory_map_ptr: u64,
+    pub memory_map_len: u64,
+
+    pub framebuffer_base: u64,
+    pub framebuffer_width: u32,
+    pub framebuffer_height: u32,
+    pub framebuffer_stride: u32,
+    pub framebuffer_format: PixelFormat,
+
+    pub kernel_phys_start: u64,
+    pub kernel_phys_end: u64,
+
+    pub initramfs_start: u64,
+    pub initramfs_len: u64,
+
+    pub rsdp_addr: u64,
+}
+
+#[repr(u32)]
+pub enum PixelFormat {
+    RedGreenBlue = 0,
+    BlueGreenRed = 1,
+    PixelBitMask = 2,
+    PixelBltOnly = 3,
+    PixelFormatMax = 4,
+}
+
+#[repr(C)]
+pub struct MemoryRegion {
+    pub start: u64,
+    pub len: u64,
+    pub region_type: u32,
+}
