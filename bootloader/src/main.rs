3b194b55fc68b0c46b4f8d87a61095b72be4cd88
yusa129be <yusawatanabe8@gmail.com>
2026-01-12 10:00:00 +0900

    boot: load ELF PT_LOAD segments into memory

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -2,14 +2,48 @@
 #![no_main]
 #![feature(abi_efiapi)]
 
+mod elf_loader;
+mod handoff;
+mod memory_map;
 mod uefi;
 
 use core::ffi::c_void;
+use elf_loader::{validate_elf, program_headers, Elf64Header, PT_LOAD};
+use handoff::BootInfo;
 use uefi::SystemTable;
 
 #[panic_handler]
 fn panic(_info: &core::panic::Panick_info) -> ! {
     loop {}
+}
+
+fn load_kernel(data: &[u8]) -> Option<u64> {
+    let header = unsafe { &*(data.as_ptr() as *const Elf64Header) };
+    if !validate_elf(header) {
+        return None;
+    }
+    let phdrs = program_headers(header);
+    for ph in phdrs {
+        if ph.p_type == PT_LOAD {
+            let start = ph.p_vaddr;
+            let end = start + ph.p_memsz;
+            let file_start = ph.p_offset as usize;
+            let file_end = file_start + ph.p_filesz as usize;
+            if file_end > data.len() {
+                return None;
+            }
+            let src = &data[file_start..file_end];
+            let dst = start as *mut u8;
+            unsafe {
+                core::ptr::copy_nonoverlapping(src.as_ptr(), dst, src.len());
+                if ph.p_memsz > ph.p_filesz {
+                    let bss = dst.add(ph.p_filesz as usize);
+                    core::ptr::write_bytes(bss, 0, (ph.p_memsz - ph.p_filesz) as usize);
+                }
+            }
+        }
+    }
+    Some(header.e_entry)
 }
 
 #[export_name = "efi_main"]
