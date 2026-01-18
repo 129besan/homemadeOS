2e86a295affebce63d7fa026467a7f5396683742
yusa129be <yusawatanabe8@gmail.com>
2026-01-18 10:00:00 +0900

    kernel: accept BootInfo at entry

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,12 +1,38 @@
 #![no_std]
 #![no_main]
 
+use core::ffi::c_void;
+
+#[repr(C)]
+pub struct BootInfo {
+    pub memory_map_ptr: u64,
+    pub memory_map_len: u64,
+    pub framebuffer_base: u64,
+    pub framebuffer_width: u32,
+    pub framebuffer_height: u32,
+    pub framebuffer_stride: u32,
+    pub framebuffer_format: u32,
+    pub kernel_phys_start: u64,
+    pub kernel_phys_end: u64,
+    pub initramfs_start: u64,
+    pub initramfs_len: u64,
+    pub rsdp_addr: u64,
+}
+
+pub static mut BOOT_INFO: Option<&'static BootInfo> = None;
+
+mod arch;
+
 #[panic_handler]
 fn panic(_info: &core::panic::Panick_info) -> ! {
     loop {}
 }
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn _start(boot_info: &'static BootInfo) -> ! {
+    unsafe {
+        BOOT_INFO = Some(boot_info);
+    }
+    arch::boot::init();
     loop {}
 }
