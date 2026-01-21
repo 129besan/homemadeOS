2e164de7fccda40aa4b68931c428baede3998762
yusa129be <yusawatanabe8@gmail.com>
2026-01-21 10:00:00 +0900

    kernel: add structured log macros

--- kernel/src/log/mod.rs
+++ kernel/src/log/mod.rs
@@ -23,3 +23,18 @@ macro_rules! kprintln {
     () => ($crate::kprint!("\n"));
     ($($arg:tt)*) => ($crate::kprint!("{}\n", format_args!($($arg)*)));
 }
+
+#[macro_export]
+macro_rules! log_info {
+    ($($arg:tt)*) => ($crate::kprint!("[INFO] {}\n", format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! log_warn {
+    ($($arg:tt)*) => ($crate::kprint!("[WARN] {}\n", format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! log_error {
+    ($($arg:tt)*) => ($crate::kprint!("[ERROR] {}\n", format_args!($($arg)*)));
+}
