5629eb2a0688c5cbe1dd43447ca8d6726ae0fd25
yusa129be <yusawatanabe8@gmail.com>
2026-01-25 11:00:00 +0900

    mm: reserve boot info and framebuffer regions

--- kernel/src/mm/frame_allocator.rs
+++ kernel/src/mm/frame_allocator.rs
@@ -1,4 +1,6 @@
 use crate::mm::addr::{PhysAddr, PhysFrame, PAGE_SIZE};
+use crate::mm::memory_map::MemoryRegion;
+use crate::BootInfo;
 
 pub struct FrameAllocator {
     bitmap: &'static mut [u64],
@@ -26,6 +28,22 @@ impl FrameAllocator {
     pub fn reserve_kernel(&mut self, phys_start: PhysAddr, phys_end: PhysAddr) {
         let len = (phys_end.0 - phys_start.0) as usize;
         self.reserve_region(phys_start, len);
+    }
+
+    pub fn reserve_boot_info(&mut self, boot_info: &BootInfo) {
+        let bi_addr = PhysAddr(boot_info as *const BootInfo as u64);
+        self.reserve_region(bi_addr, core::mem::size_of::<BootInfo>());
+
+        let fb_base = PhysAddr(boot_info.framebuffer_base);
+        let fb_size = (boot_info.framebuffer_stride as u64 * boot_info.framebuffer_height as u64) as usize;
+        if fb_base.0 != 0 {
+            self.reserve_region(fb_base, fb_size);
+        }
+
+        if boot_info.memory_map_ptr != 0 {
+            let mmap_size = boot_info.memory_map_len as usize * core::mem::size_of::<MemoryRegion>();
+            self.reserve_region(PhysAddr(boot_info.memory_map_ptr), mmap_size);
+        }
     }
 
     fn set_used(&mut self, frame: usize) {
