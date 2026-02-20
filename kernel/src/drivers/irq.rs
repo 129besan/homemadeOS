40a90c9326c395e88a0a7b93b707d1971d2c954e
yusa129be <yusawatanabe8@gmail.com>
2026-02-20 10:00:00 +0900

    interrupts: add IRQ dispatch table

---
+++ kernel/src/drivers/irq.rs
@@ -0,0 +1,34 @@
+use crate::arch::x86_64::idt::Idt;
+
+const PIC_REMAP_OFFSET: u8 = 0x20;
+
+type IrqHandler = fn();
+
+static mut IRQ_HANDLERS: [Option<IrqHandler>; 16] = [None; 16];
+
+pub fn register_irq(irq: u8, handler: IrqHandler) {
+    if irq < 16 {
+        unsafe {
+            IRQ_HANDLERS[irq as usize] = Some(handler);
+        }
+    }
+}
+
+extern "x86-interrupt" fn irq_handler(frame: &mut crate::arch::x86_64::interrupts::InterruptFrame) {
+    let irq = (frame.cs >> 8) & 0xff;
+    if irq < 16 {
+        if let Some(handler) = unsafe { IRQ_HANDLERS[irq as usize] } {
+            handler();
+        }
+    }
+}
+
+pub fn init_irqs(idt: &mut Idt) {
+    let ks = 0x08;
+    let present = 0x8e;
+    for i in 0..16 {
+        idt.entries[(PIC_REMAP_OFFSET as usize) + i].set_handler(
+            irq_handler as u64, ks, present,
+        );
+    }
+}
--- kernel/src/drivers/mod.rs
+++ kernel/src/drivers/mod.rs
@@ -1,3 +1,4 @@
+pub mod irq;
 pub mod keyboard;
 pub mod pic;
 pub mod pit;
