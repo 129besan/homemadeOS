634abff177de575d6c92b6f5a7477f5faa060ea3
yusa129be <yusawatanabe8@gmail.com>
2026-03-25 10:00:00 +0900

    proc: add process table

--- kernel/src/proc/process.rs
+++ kernel/src/proc/process.rs
@@ -1,4 +1,6 @@
 use crate::sched::task::Pid;
+use crate::mm::paging::address_space::AddressSpace;
+use alloc::collections::BTreeMap;
 
 pub enum ProcessState {
     Alive,
@@ -8,6 +10,40 @@ pub enum ProcessState {
 pub struct Process {
     pub pid: Pid,
     pub state: ProcessState,
+    pub address_space: Option<AddressSpace>,
+}
+
+pub struct ProcessTable {
+    pub processes: BTreeMap<Pid, Process>,
+    pub next_pid: u64,
+}
+
+impl ProcessTable {
+    pub fn new() -> Self {
+        ProcessTable {
+            processes: BTreeMap::new(),
+            next_pid: 1,
+        }
+    }
+
+    pub fn allocate_pid(&mut self) -> Pid {
+        let pid = Pid(self.next_pid);
+        self.next_pid += 1;
+        pid
+    }
+
+    pub fn insert(&mut self, process: Process) {
+        let pid = process.pid;
+        self.processes.insert(pid, process);
+    }
+
+    pub fn get(&self, pid: Pid) -> Option<&Process> {
+        self.processes.get(&pid)
+    }
+
+    pub fn get_mut(&mut self, pid: Pid) -> Option<&mut Process> {
+        self.processes.get_mut(&pid)
+    }
 }
 
 pub fn kill_user_task(pid: Pid) {
