a509c2f6aa87a92fbd77c1270c76c657938ec1fe
yusa129be <yusawatanabe8@gmail.com>
2026-04-02 10:00:00 +0900

    userspace: add echo program

---
+++ userspace/echo/src/main.rs
@@ -0,0 +1,14 @@
+#![no_std]
+#![no_main]
+
+use libc_lite::write;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
